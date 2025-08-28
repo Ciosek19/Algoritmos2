c86b99f42d687dc2d5eff74f345099a48d4d5a69
ciosek19 <rodrigociosek@gmail.com>
2025-08-27 22:21:51 -0300

    Practico listas enlazadas

---
+++ Practico2A_ListaDoblementeEnlazadas/src/ListaDobleEnlace.java
@@ -0,0 +1,59 @@
+public class ListaDobleEnlace {
+    public Nodo cabeza;
+    public int largo;
+
+
+
+    public ListaDobleEnlace(){
+        largo = 0;
+    }
+
+    public void EliminarLista(){
+        this.cabeza = null;
+    }
+
+    public void insertarNodoAlInicio(int valor){
+        Nodo nuevo = new Nodo(valor);
+        if (largo == 0){
+            cabeza = nuevo;
+            largo++;
+            return;
+        }
+        nuevo.siguiente = this.cabeza;
+        this.cabeza.anterior = nuevo;
+        this.cabeza = nuevo;
+        largo++;
+    }
+
+    public int valorCabeza(){
+        return cabeza.valor;
+    }
+
+    public void insertarNodoAlFinal(int valor){
+        Nodo nuevo = new Nodo(valor);
+        if (largo == 0){
+            this.cabeza = nuevo;
+            largo++;
+            return;
+        }
+        Nodo aux = cabeza;
+        while(aux.siguiente != null){
+            aux = aux.siguiente;
+        }
+        nuevo.anterior = aux;
+        aux.siguiente = nuevo;
+    }
+
+    public void insertarValorEnPosicion(int pos, int valor){
+        if (pos > largo || pos < 1) return;
+        Nodo nuevo = new Nodo(valor);
+        Nodo aux = cabeza;
+        int indice = 1;
+        while(indice != pos){
+            aux = aux.siguiente;
+            indice++;
+        }
+        
+        //1 -> 2 -> 3 -> 4 (pos 2)
+    }
+}
---
+++ Practico2A_ListaDoblementeEnlazadas/src/Nodo.java
@@ -0,0 +1,9 @@
+public class Nodo {
+    public int valor;
+    public Nodo siguiente;
+    public Nodo anterior;
+
+    public Nodo(int valor){
+        this.valor = valor;
+    }
+}
